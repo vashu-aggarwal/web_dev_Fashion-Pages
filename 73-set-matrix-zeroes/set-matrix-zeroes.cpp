efd585018c87b60abdd0eb2fdebe984a8173c0f0
vashu-aggarwal <138757578+vashu-aggarwal@users.noreply.github.com>
2024-07-15 18:44:36 +0530

    Time: 9 ms (58.64%) | Memory: 16.9 MB (14.19%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<pair<int, int>> v;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    v.push_back({i, j});
+                }
+            }
+        }
+
+      
+        for (int i = 0; i < v.size(); i++) {
+            int row = v[i].first;
+            int col = v[i].second;
+
+           
+            for (int k = 0; k < m; k++) {
+                matrix[row][k] = 0;
+            }
+
+            
+            for (int k = 0; k < n; k++) {
+                matrix[k][col] = 0;
+            }
+        }
+    }
+};
